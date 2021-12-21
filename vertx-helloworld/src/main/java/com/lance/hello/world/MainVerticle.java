5cf5e1afcd587cacbf0f2cc3dff269406ecd97a2
lance <lh160@zcsmart.com>
2021-12-21 11:08:41 +0800

    update port

--- vertx-helloworld/src/main/java/com/lance/hello/world/MainVerticle.java
+++ vertx-helloworld/src/main/java/com/lance/hello/world/MainVerticle.java
@@ -13,11 +13,11 @@ public class MainVerticle extends AbstractVerticle {
 
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-          .putHeader("content-type", "text/plain")
-          .end("Hello from Vert.x!");
-    }).listen(8888, http -> {
+    vertx.createHttpServer().requestHandler(req ->
+        req.response()
+            .putHeader("content-type", "text/plain")
+            .end("Hello from Vert.x!")
+    ).listen(8888, http -> {
       if (http.succeeded()) {
         startPromise.complete();
         System.out.println("HTTP server started on port 8888");
