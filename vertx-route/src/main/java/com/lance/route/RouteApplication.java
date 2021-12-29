402b8b5e155a7022dc2f688197806bdcdf7a1ad8
lance <li6151770@126.com>
2021-12-29 10:20:27 +0800

    add vertx-route

---
+++ vertx-route/src/main/java/com/lance/route/RouteApplication.java
@@ -0,0 +1,14 @@
+package com.lance.route;
+
+/**
+ * Route application
+ *
+ * @author lance
+ * @date 2021/12/24 22:01
+ */
+public class RouteApplication {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ vertx-route/src/test/java/com/lance/route/RouteApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lance.route;
+
+import org.junit.jupiter.api.Test;
+
+class RouteApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
