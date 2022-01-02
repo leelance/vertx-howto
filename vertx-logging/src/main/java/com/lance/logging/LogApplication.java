cfce71f58396868427a82674f267ecf345c6f5a4
lance <li6151770@126.com>
2022-01-03 00:09:04 +0800

    add log4j2

--- vertx-logging/src/main/java/com/lance/logging/LogApplication.java
+++ vertx-logging/src/main/java/com/lance/logging/LogApplication.java
@@ -11,7 +11,6 @@ import io.vertx.core.Vertx;
 public class LogApplication {
 
   public static void main(String[] args) {
-    System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
     Vertx vertx = Vertx.vertx();
     vertx.deployVerticle(MainVerticle.class.getName());
   }
