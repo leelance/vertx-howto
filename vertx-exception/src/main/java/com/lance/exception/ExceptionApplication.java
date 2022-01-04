5610a881edade86b1cade41e378ebf34889a8c1d
lihao <li6151770@126.com>
2022-01-04 20:55:59 +0800

    add exception module

---
+++ vertx-exception/src/main/java/com/lance/exception/ExceptionApplication.java
@@ -0,0 +1,14 @@
+package com.lance.exception;
+
+/**
+ * Exception application
+ *
+ * @author lance
+ * @date 2022/1/4 20:52
+ */
+public class ExceptionApplication {
+
+	public static void main(String[] args) {
+
+	}
+}
---
+++ vertx-exception/src/test/java/com/lance/exception/ExceptionApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lance.exception;
+
+import org.junit.jupiter.api.Test;
+
+class ExceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
