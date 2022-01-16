b8beba9187182fd57a60d6146fe745700a620eae
lance <li6151770@126.com>
2022-01-16 10:09:27 +0800

    add metric module

---
+++ vertx-metrics/src/main/java/com/lance/metrics/vertxmetrics/MetricsApplication.java
@@ -0,0 +1,13 @@
+package com.lance.metrics.vertxmetrics;
+
+/**
+ * Metrics application
+ *
+ * @author lance
+ * @date 2022/1/16 10:05
+ */
+public class MetricsApplication {
+
+  public static void main(String[] args) {
+  }
+}
---
+++ vertx-metrics/src/test/java/com/lance/metrics/vertxmetrics/MetricsApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lance.metrics.vertxmetrics;
+
+import org.junit.jupiter.api.Test;
+
+class MetricsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
