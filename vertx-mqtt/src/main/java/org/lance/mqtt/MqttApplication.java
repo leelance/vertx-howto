2299a8cc216a5379aeecd981e8999b0b582e77bc
lance <li6151770@126.com>
2022-01-21 20:01:03 +0800

    add mqtt module

---
+++ vertx-mqtt/src/main/java/org/lance/mqtt/MqttApplication.java
@@ -0,0 +1,14 @@
+package org.lance.mqtt;
+
+/**
+ * MQTT application
+ *
+ * @author lance
+ * @date 2022/1/21 19:20
+ */
+public class MqttApplication {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ vertx-mqtt/src/test/java/org/lance/mqtt/MqttApplicationTests.java
@@ -0,0 +1,11 @@
+package org.lance.mqtt;
+
+import org.junit.jupiter.api.Test;
+
+class MqttApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
