284bee4637fedf24b8103c12ef4ec02fddfe53d4
lance <li6151770@126.com>
2022-02-13 12:27:17 +0800

    add rabbit mq

---
+++ vertx-rabbitmq/src/main/java/com/lance/rabbit/RabbitmqApplication.java
@@ -0,0 +1,14 @@
+package com.lance.rabbit;
+
+/**
+ * Rabbitmq application
+ *
+ * @author lance
+ * @date 2022/2/13 12:26
+ */
+public class RabbitmqApplication {
+
+  public static void main(String[] args) {
+
+  }
+}
---
+++ vertx-rabbitmq/src/test/java/com/lance/rabbit/RabbitmqApplicationTests.java
@@ -0,0 +1,11 @@
+package com.lance.rabbit;
+
+import org.junit.jupiter.api.Test;
+
+class RabbitmqApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
