665e761644287aa26652583ba377011b0a181cb8
lance <li6151770@126.com>
2022-02-13 15:13:26 +0800

    add grpc module

---
+++ vertx-grpc/src/main/java/org/lance/grpc/GrpcApplication.java
@@ -0,0 +1,11 @@
+package org.lance.grpc;
+
+/**
+ * Grpc application
+ *
+ * @author lance
+ * @date 2022/2/13 15:11
+ */
+public class GrpcApplication {
+
+}
---
+++ vertx-grpc/src/test/java/org/lance/grpc/GrpcApplicationTests.java
@@ -0,0 +1,11 @@
+package org.lance.grpc;
+
+import org.junit.jupiter.api.Test;
+
+class GrpcApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
